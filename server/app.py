57fde369ef29b5c3a6518adfd695d22d87a5ad59
ykentrell545@gmail.com <xokentr3ll@DESKTOP-4BEPFC0>
2024-10-04 20:49:48 -0700

    adds imports statements for jsonify in app.py

--- server/app.py
+++ server/app.py
@@ -6,6 +6,9 @@ from flask_migrate import Migrate
 
 from models import db, Earthquake
 
+from flask import Flask, make_response, jsonify  # Add jsonify here
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,6 +24,51 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake_by_id(id):
+    earthquake = Earthquake.query.get(id)
+    
+    if earthquake:
+        return jsonify({
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }), 200
+    else:
+        return jsonify({'message': f'Earthquake {id} not found.'}), 404
+
+
+        # Add view to get earthquakes by minimum magnitude
+
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    
+    quake_list = [
+        {
+            'id': quake.id,
+            'location': quake.location,
+            'magnitude': quake.magnitude,
+            'year': quake.year
+        } for quake in earthquakes
+    ]
+    
+    return jsonify({
+        'count': len(quake_list),
+        'quakes': quake_list
+    }), 200
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
+
+
+
+
 
 
 if __name__ == '__main__':
